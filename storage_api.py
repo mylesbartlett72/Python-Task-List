bc9ee503cf3d0370dae1ae1e1d14de52037d4988
Myles <myles@bangsparks.com>
2022-11-04 18:25:14 +0000

    Autoformat files

--- storage_api.py
+++ storage_api.py
@@ -2,14 +2,17 @@ import json
 
 DEFAULT_FILE_CONTENT = '{"To Do": [], "In Progress": [], "Done": []}'
 
-def get_data(filename = ".tasks.json"):
+
+def get_data(filename=".tasks.json"):
     with open(filename, "r") as tasks:
         return json.load(tasks)
 
-def write_data(data, filename = ".tasks.json"):
+
+def write_data(data, filename=".tasks.json"):
     with open(filename, "w") as tasks:
         json.dump(data, tasks)
 
-def create_file(filename = ".tasks.json"):
+
+def create_file(filename=".tasks.json"):
     with open(filename, "w") as tasks:
         tasks.write(DEFAULT_FILE_CONTENT)
